6362f83aba06044fa40b98a5126422753435827a
BENNY B <benb@BENNYs-MacBook-Air.local>
2021-07-21 02:22:18 -0400

    first commit

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'validations.php';
+
+require_login(); 
+// connect to db
+require_once 'database.php';
+$conn = db_connect();
+?>
+
+<?php
+
+include_once 'shared/top.php';
+
+?>
+
+<img src="img/w.jpg" class="img-fluid" alt="">
+
+<?php
+
+include_once 'shared/footer.php';
+
+?>
